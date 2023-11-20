6d54c3a0559fe4c98d71bbd9cd74fecfa98b9314
Bruno Guratti <123127842+brunoguratti@users.noreply.github.com>
2023-11-19 21:36:29 -0500

    rf

--- crimela_prediction.py
+++ crimela_prediction.py
@@ -3,7 +3,7 @@ import pickle as cPickle
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForstClassifier
 
 
 # Load any compressed pickle file
